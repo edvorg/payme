090dc8f08537cd15d8be58d8dc845d8cf50cca25
Edward Knyshov <edvorg@gmail.com>
2018-03-30 15:16:05 +0700

    remove newlines when pasting data in textarea

--- web-app/src/payme/core.cljs
+++ web-app/src/payme/core.cljs
@@ -182,9 +182,9 @@
        [:label "Any notes for terms and agreements section?"]
        [:label "(Ctrl-Enter for line break, empty line for paragraph)"]
        [:textarea {:type :string
-                   :value (clojure.string/replace @terms "<ENTER>" "\n")
+                   :value (s/replace @terms "<ENTER>" "\n")
                    :on-change (fn [e]
-                                (reset! terms (.. e -target -value)))
+                                (reset! terms (s/replace (.. e -target -value) "\n" "<ENTER>")))
                    :auto-focus true
                    :on-key-press (on-enter #(do
                                               (show-message [number-view])
