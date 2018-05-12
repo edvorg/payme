0727c3f0627d2fbd8edcd53f560ec63546651874
Edward Knyshov <edvorg@gmail.com>
2018-05-12 22:03:56 +0700

    start redis on host

--- start-redis.sh
+++ start-redis.sh
@@ -2,4 +2,4 @@
 
 DATA=`realpath ./data`
 mkdir -p "${DATA}"
-docker run --name payme-redis-dev -v "${DATA}:/data" -p 6379:6379 -d redis:3.2 redis-server --appendonly yes
+docker run --name payme-redis-dev -v "${DATA}:/data" --net host -d redis:3.2 redis-server --appendonly yes
